69e743fd15c525773442c87d604ccae923113d91
Stefan Peterson <stefan.peterson@rubico.com>
2015-10-08 18:07:50 +0200

    Changes to setup.py for pypi purposes

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(name="planfftw",
       author_email="stefan.j.peterson@gmail.com",
       url="https://github.com/sjpet/planfftw",
       download_url="https://github.com/sjpet/planfftw/tarball/0.1",
-      requires=["pyfftw>=0.9.2",        # Probably lower versions
-                "numpy>=1.8.2"],        # Probably lower versions
-      testing_requires=["pytest>=2.8.2"],       # Probably lower versions
+#      requires=["pyfftw>=0.9.2",        # Probably lower versions
+#                "numpy>=1.8.2"],        # Probably lower versions
+#      testing_requires=["pytest>=2.8.2"],       # Probably lower versions
       keywords=["pyfftw", "fft", "fftw", "plan", "planner"],
       classifiers=[])
 
