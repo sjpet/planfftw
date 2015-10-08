1decd3ec12270e7a073be88f47b9b37560b09242
Stefan Peterson <stefan.peterson@rubico.com>
2015-10-08 18:24:19 +0200

    Fixing namespace issues

--- planfftw/__init__.py
+++ planfftw/__init__.py
@@ -1,4 +1,13 @@
 #!usr/bin/env python
-from .planners import *
+from .planners import (fft, 
+                       rfft, 
+                       fftn,
+                       rfftn,
+                       ifft,
+                       irfft,
+                       ifftn,
+                       irfftn,
+                       firfilter,
+                       correlate)
 
 __version__ = '0.1'
