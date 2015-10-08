39b461b30a6d9349bffb953ac7e4e79dd5bdce4f
Stefan Peterson <stefan.peterson@rubico.com>
2015-10-08 17:44:35 +0200

    Added tests for fft planners

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(name="planfftw",
       author_email="stefan.j.peterson@gmail.com",
       url="https://github.com/sjpet/planfftw",
       download_url="https://github.com/sjpet/planfftw/tarball/0.1",
-      requires=['numpy'],
-      install_requires=["pyfftw>=0.9.2",
-                        "numpy>=1.8.2"],
+      install_requires=["pyfftw>=0.9.2",        # Probably lower versions
+                        "numpy>=1.8.2"],        # Probably lower versions
+      testing_requires=["pytest>=2.8.2"],       # Probably lower versions
       keywords=["pyfftw", "fft", "fftw", "plan", "planner"],
       classifiers = [])
 
